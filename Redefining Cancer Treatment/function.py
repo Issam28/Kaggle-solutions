07bbb11fb72f10cac4ff8e43877128cbb894425d
Issam <di_mahmoudi@esi.dz>
2023-02-13 17:46:49 +0100

    initial update

---
+++ Redefining Cancer Treatment/function.py
@@ -0,0 +1,17 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+import sklearn
+
+class cust_regression_vals(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
+    def fit(self, x, y=None):
+        return self
+    def transform(self, x):
+        x = x.drop(['Gene','Text', 'Variation','ID',],axis=1).values
+        return x
+
+class cust_txt_col(sklearn.base.BaseEstimator, sklearn.base.TransformerMixin):
+    def __init__(self, key):
+        self.key = key
+    def fit(self, x, y=None):
+        return self
+    def transform(self, x):
+        return x[self.key].apply(str)
